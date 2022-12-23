c81deb7788bc6dac187b188de7cb66e527178793
willyGhosly <35883969+willyGhosly@users.noreply.github.com>
2022-12-22 23:51:18 -0500

    correct plugin autor name

--- altillo-blocks.php
+++ altillo-blocks.php
@@ -7,7 +7,7 @@
  * Version          : 0.0.1
  * Requires at least: 5.2
  * Requires PHP     : 7.2
- * Author           : Wily Ghostly
+ * Author           : Willy Ghostly
  * Author URI       : https://desdeelaltillo.com/
  * License          : GPL v2 or later
  * License URI      : https://www.gnu.org/licenses/gpl-2.0.html
